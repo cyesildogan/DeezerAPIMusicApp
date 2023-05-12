cd9b30c1ca8b2d676f8208a1a763a2a75e2ca6ba
cyesildogan <can.yesildogan@gmail.com>
2023-05-12 18:38:34 +0300

    create adapter

---
+++ app/src/main/java/com/example/deezerapimusicapp/adapter/ArtistListAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.deezerapimusicapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.deezerapimusicapp.R
+import com.example.deezerapimusicapp.databinding.AritstsListRecyclerRowBinding
+import com.example.deezerapimusicapp.model.artist.ArtistData
+import com.example.deezerapimusicapp.util.loadUrl
+
+class ArtistListAdapter(
+    private val artistList: List<ArtistData>,
+    private val onClickListener: OnClickListener
+) : ListAdapter<ArtistData, ArtistListAdapter.ViewHolder>(DiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.aritsts_list_recycler_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return artistList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(artistList[position])
+        holder.itemView.setOnClickListener {
+            onClickListener.clickListener(artistList[position])
+        }
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = AritstsListRecyclerRowBinding.bind(itemView)
+        fun bind(artist: ArtistData){
+            artist.name.let {
+                binding.artistListRecyclerArtistsNameText.text = it.toString()
+            }
+            artist.picture_big.let {
+                binding.artistListRecyclerArtistImageView.loadUrl(it)
+            }
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<ArtistData>() {
+        override fun areItemsTheSame(oldItem: ArtistData, newItem: ArtistData): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: ArtistData, newItem: ArtistData): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    class OnClickListener(val clickListener: (artistList: ArtistData) -> Unit) {
+        fun onClick(artist: ArtistData) = clickListener(artist)
+    }
+}
---
+++ app/src/main/java/com/example/deezerapimusicapp/adapter/GenreListAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.deezerapimusicapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.deezerapimusicapp.R
+import com.example.deezerapimusicapp.databinding.CategoriesRecyclerRowBinding
+import com.example.deezerapimusicapp.model.genre.GenreData
+import com.example.deezerapimusicapp.util.loadUrl
+
+
+class GenreListAdapter(
+    private val genreList: List<GenreData>,
+    private val onClickListener: OnClickListener
+) : ListAdapter<GenreData, GenreListAdapter.ViewHolder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.categories_recycler_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return genreList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(genreList[position])
+        holder.itemView.setOnClickListener {
+            onClickListener.clickListener(genreList[position])
+
+        }
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = CategoriesRecyclerRowBinding.bind(itemView)
+        fun bind(genre: GenreData) {
+           genre.picture_big.let {
+               binding.categoriesRecyclerCategoriesImageView.loadUrl(it)
+           }
+            genre.name
+                .let { binding.categoriesRecyclerCategoriesNameTextView
+                    .text = it.toString() }
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<GenreData>() {
+        override fun areItemsTheSame(oldItem: GenreData, newItem: GenreData): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: GenreData, newItem: GenreData): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    class OnClickListener(val clickListener: (genreList: GenreData) -> Unit) {
+        fun onClick(genre: GenreData) = clickListener(genre)
+    }
+}
